7f4aeeefde2ab28f981cc3f2a3f307f01f1edcfe
urmsori <urmsori@gmail.com>
2025-09-26 19:24:49 +0900

    Update component.h

--- c/fsw/compile_time_polymorphism/example/component/component.h
+++ c/fsw/compile_time_polymorphism/example/component/component.h
@@ -4,14 +4,6 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
-#if (VARIANT_COMPONENT == a)
-#include <component/variant_define/a/component_define.h>
-#elif (VARIANT_COMPONENT == b)
-#include <component/variant_define/b/component_define.h>
-#else
-#error "VARIANT_COMPONENT undefinded"
-#endif
-
 #include <stdint.h>
 
 uint32_t component_read(void);
