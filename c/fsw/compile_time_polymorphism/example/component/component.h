7344685ad47c5e8c1e3b75c8412e9a37c76df2a4
urmsori <urmsori@gmail.com>
2025-09-26 19:20:07 +0900

    Update component.h

--- c/fsw/compile_time_polymorphism/example/component/component.h
+++ c/fsw/compile_time_polymorphism/example/component/component.h
@@ -1,3 +1,6 @@
+#include <component/variant_define/component_variant_define.h>
+#ifdef COMPONENT_DEFINE_H
+
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
@@ -15,3 +18,5 @@ uint32_t component_read(void);
 void component_write(uint8_t data[COMPONENT_WRITE_DATA_SIZE_MAX]);
 
 #endif // COMPONENT_H
+
+#endif // COMPONENT_DEFINE_H
