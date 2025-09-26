11191d6cbe5cbc6c2a28bd1b6a9dfe509e0120bf
urmsori <urmsori@gmail.com>
2025-09-26 19:44:29 +0900

    Update component_a.h

--- c/fsw/compile_time_polymorphism/example/component/variant_specific/a/component_a.h
+++ c/fsw/compile_time_polymorphism/example/component/variant_specific/a/component_a.h
@@ -1,8 +1,12 @@
+#include <component/component.h>
+#ifdef COMPONENT_H
+
 #ifndef COMPONENT_A_H
 #define COMPONENT_A_H
 
-#include <component/component.h>
 
 void component_a_reset(void);
 
 #endif // COMPONENT_A_H
+
+#endif // COMPONENT_H
