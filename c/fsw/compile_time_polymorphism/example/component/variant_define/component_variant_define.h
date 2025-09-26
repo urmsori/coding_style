72d290d685e5ea276b0fdd762b603bf76a2b60b6
urmsori <urmsori@gmail.com>
2025-09-26 19:18:47 +0900

    Create component_variant_define.h

---
+++ c/fsw/compile_time_polymorphism/example/component/variant_define/component_variant_define.h
@@ -0,0 +1,10 @@
+#ifndef COMPONENT_VARIANT_DEFINE_H
+#define COMPONENT_VARITAN_DEFINE_H
+
+#if (VARIANT_COMPONENT == a)
+#include <component/variant_define/a/component_define.h>
+#elif (VARIANT_COMPONENT == b)
+#include <component/variant_define/b/component_define.h>
+#endif
+
+#endif // COMPONENT_VARIANT_DEFINE_H
