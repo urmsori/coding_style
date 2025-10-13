ff8fcd6627e22403b92e9d48b6e55df278af109d
urmsori <urmsori@gmail.com>
2025-10-13 18:14:58 +0900

    Update component_variant_define.h

--- c/fsw/compile_time_polymorphism/example/component/variant_define/component_variant_define.h
+++ c/fsw/compile_time_polymorphism/example/component/variant_define/component_variant_define.h
@@ -1,10 +1,16 @@
 #ifndef COMPONENT_VARIANT_DEFINE_H
-#define COMPONENT_VARITAN_DEFINE_H
+#define COMPONENT_VARIANT_DEFINE_H
 
-#if (VARIANT_COMPONENT == a)
-#include <component/variant_define/a/component_define.h>
-#elif (VARIANT_COMPONENT == b)
-#include <component/variant_define/b/component_define.h>
+#define COMPONENT_VARIANT_DEFINE_HEADER <component/variant_define/VARIANT_COMPONENT/component_define.h>
+#if __has_include (COMPONENT_VARIANT_DEFINE_HEADER)
+#include COMPONENT_VARIANT_DEFINE_HEADER
 #endif
+
+// if not -std=gnu2x
+// #if (VARIANT_COMPONENT == a)
+// #include <component/variant_define/a/component_define.h>
+// #elif (VARIANT_COMPONENT == b)
+// #include <component/variant_define/b/component_define.h>
+// #endif
 
 #endif // COMPONENT_VARIANT_DEFINE_H
