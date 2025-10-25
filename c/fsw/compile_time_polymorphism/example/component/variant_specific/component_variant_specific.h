a253d96e9dcbc700e6a64f9384b93c676992cc10
urmsori <urmsori@gmail.com>
2025-10-25 13:28:55 +0900

    Add header for variant-specific component

---
+++ c/fsw/compile_time_polymorphism/example/component/variant_specific/component_variant_specific.h
@@ -0,0 +1,17 @@
+#ifndef COMPONENT_VARIANT_SPECIFIC_H_
+#define COMPONENT_VARIANT_SPECIFIC_H_
+
+#include <component/component.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  void component_reset(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMPONENT_VARIANT_SPECIFIC_H_
