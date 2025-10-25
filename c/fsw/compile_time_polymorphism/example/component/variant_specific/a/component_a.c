cc1eb21331ea4a3cbed0181b6e009bcedb6ead18
urmsori <urmsori@gmail.com>
2025-10-25 13:34:03 +0900

    Refactor include guards and headers in component_a.c
    Reorganized include guards and header file includes.

--- c/fsw/compile_time_polymorphism/example/component/variant_specific/a/component_a.c
+++ c/fsw/compile_time_polymorphism/example/component/variant_specific/a/component_a.c
@@ -1,6 +1,7 @@
-#include "component/variant_specific/a/component_a.h"
 #include "component/variant_specific/component_variant_specific.h"
-#ifdef COMPONENT_A_H
+#ifdef COMPONENT_VARIANT_SPECIFIC_H_
+
+#include "component/variant_specific/a/component_a.h"
 
 void component_write(uint8_t data[COMPONENT_WRITE_DATA_SIZE_MAX]){
     // Implementation of component_write with a
@@ -15,4 +16,4 @@ void component_reset(void){
     component_a_reset();
 }
 
-#endif
+#endif // COMPONENT_VARIANT_SPECIFIC_H_
