1b1daab6309c59731f3f72c1dfdb16a543717784
urmsori <urmsori@gmail.com>
2025-10-25 13:34:46 +0900

    Update component.c

--- c/fsw/compile_time_polymorphism/example/component/component.c
+++ c/fsw/compile_time_polymorphism/example/component/component.c
@@ -1,7 +1,10 @@
 #include "component/component.h"
 #ifdef COMPONENT_H
 
+#include "component/variant_specific/component_variant_specific.h"
+
 uint32_t component_read(void){
+    component_reset();
     return 0;
 }
 
