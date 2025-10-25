6c371fe55d8bcda140b327dd5ba0b20b0194fa96
urmsori <urmsori@gmail.com>
2025-10-25 13:33:29 +0900

    Modify component_b.c for variant-specific handling
    Updated includes and added component_reset function.

--- c/fsw/compile_time_polymorphism/example/component/variant_specific/b/component_b.c
+++ c/fsw/compile_time_polymorphism/example/component/variant_specific/b/component_b.c
@@ -1,8 +1,12 @@
-#include "component/component.h"
-#ifdef COMPONENT_H
+#include "component/variant_specific/component_variant_specific.h"
+#ifdef COMPONENT_VARIANT_SPECIFIC_H_
 
 void component_write(uint8_t data[COMPONENT_WRITE_DATA_SIZE_MAX]){
     // Implementation of component_write with b
 }
 
-#endif // COMPONENT_H
+void component_reset(void){
+    // Do nothting
+}
+
+#endif // COMPONENT_VARIANT_SPECIFIC_H_
